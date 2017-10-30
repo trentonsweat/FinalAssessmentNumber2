54760bf179be9978eeaa5e4e002551a638c1a80d
trentonsweat <trentonsweat@gmail.com>
2017-10-30 16:04:18 -0400

    first commit number 2... didnt have time to finish

---
+++ FinalAssessmentNumber2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalAssessmentNumber2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            NorthWindEntities ORM = new NorthWindEntities(); // define ORM
+
+            ORM.Customers.ToList();
+
+            List<Customer> CustomerList = ORM.Customers.ToList(); // select * from cusomers
+
+            ViewBag.Message = "Your application description page.";
+
+            ViewBag.Clist = CustomerList; // sending the list of customers to the view
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FinalAssessmentNumber2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalAssessmentNumber2.MvcApplication" Language="C#" %>
